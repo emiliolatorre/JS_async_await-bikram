5086520a6c72f5ed6d6c8c2495a5a776e9439721
emiliolatorre <emiliolatorreguerra@gmail.com>
2024-05-29 23:49:33 +0200

    Ejercicio 3 terminado

--- index.js
+++ index.js
@@ -43,3 +43,33 @@ getImageAndName('pikachu')
     .catch((error) => {console.error(error)})
 
 // Ejercicio 3
+const printImageAndName = async (pokemon) => {
+    try {
+        const resp = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+        if (resp.ok) {
+            const resultado = await resp.json()
+            const img = resultado.sprites.front_default;
+            const name = resultado.name
+
+            const section = document.createElement('section');
+
+            const imgDOM = document.createElement('img');
+            imgDOM.src = img
+            imgDOM.alt = name
+            
+            const nameDOM = document.createElement('h1');
+            nameDOM.innerHTML = name
+
+
+            return 'section.append(imgDOM, nameDOM)'
+        } else {
+            throw 'El estado o el stage no son correctos.'
+        }
+        } catch (error) {
+            throw 'La web no existe.'
+        }
+}
+        
+printImageAndName('pikachu')
+    .then((resp) => {console.log(resp)})
+    .catch((error) => {console.error(error)})
